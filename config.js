f03c3eab5f0b95fa816a6b7cb2eb651cd4bbdbd9
Joshua Ocrah <ocrahjoshua@gmail.com>
2021-04-19 01:55:52 +0000

    ch(config.js): load envs

---
+++ config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+if (!process.env.NODE_ENV) { process.env.NODE_ENV = 'development' }
+
+if (process.env.NODE_ENV === 'development') { require('dotenv').config() }
+
+module.exports = key => {
+    if (process.env[key] === undefined) {
+        throw new Error(`No config for env variable ${key}`)
+    }
+    return process.env[key]
+}
