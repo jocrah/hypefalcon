6231b1f8601ed130ed1f613eac6da3434ac7ba89
Joshua Ocrah <ocrahjoshua@gmail.com>
2021-04-19 01:56:47 +0000

    ch(index.js): create express server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const actions = require('./actions')
+const app = express()
+const db = require('./lib/resources/db')
+const port = 8000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+    .post('/kudos', (req, res) => {
+        actions.processData(req.body)
+        return res.status(200).send()
+    })
+
+db.connect().then(() => {
+    app.listen(port, () => console.log(`App listening on port ${port}!`))
+})
