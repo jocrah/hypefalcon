30419fbfc53e156ff5c97887c21d56fc039c8942
Joshua Ocrah <ocrahjoshua@gmail.com>
2021-05-05 07:34:42 +0000

    ch(app.js): return plaintext response to slack"

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
     .post('/kudos', async (req, res) => {
-        await actions.processData(req.body)
-        return res.status(200).send()
+        const response = await actions.processData(req.body)
+        return res.status(200).json({
+            response_type: 'in_channel',
+            text: response
+        })
     })
 
 module.exports = app
