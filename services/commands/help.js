ac67491fdd8a1eca10467613bb1a55f48fee3011
Joshua Ocrah <ocrahjoshua@gmail.com>
2021-05-06 21:33:38 +0000

    ch(services/commands/help.js): update

--- services/commands/help.js
+++ services/commands/help.js
@@ -7,5 +7,5 @@ Want to list a number of recent kudos? Do \`\kudo list <number>\`. use \`*\` in 
 Want to update a kudo? Do \`\kudo replace <kudoId> <text>\`
 Want to get ranked members in terms of kudos? Do \`\kudo top <number>\` use  \`*\` in place of \`<number>\` to get all ranks
 Want to delete a kudo? Do \`\kudo delete <kudoId>\`
-    `
+`
 }
