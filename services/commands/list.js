88be3276a903067736ef3c5a397a22a54adda685
Joshua Ocrah <ocrahjoshua@gmail.com>
2021-05-19 07:34:42 +0000

    ch(services/commands/delete.js, services/commands/replace.js, services/commands/list.js): update params

--- services/commands/list.js
+++ services/commands/list.js
@@ -1,7 +1,7 @@
 'use strict'
 const kudoModel = require('../../models/kudos')()
 
-module.exports = async text => {
+module.exports = async ({ text }) => {
     let limit
     if (text !== '*') { limit = parseInt(text) }
     return kudoModel.fetch({
