3d86d368f9c4a9e08fb340342615b36359d42699
Joshua Ocrah <ocrahjoshua@gmail.com>
2022-09-24 22:14:23 +0000

    ch(lib/resources/db.ts): update usage of mongoose after upgrade

--- lib/resources/db.ts
+++ lib/resources/db.ts
@@ -5,11 +5,7 @@ const MONGODB_URL = config('MONGODB_URL')
 
 export default {
     connect: (customMongoDBUrl?: string) => {
-        return mongoose.connect(customMongoDBUrl || MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-        })
+        return mongoose.connect(customMongoDBUrl || MONGODB_URL)
     },
     isConnected: (): boolean => mongoose.connection.readyState === 1
 }
